f541c638dd4a4da2f0c6d774b771693f96f9bf71
nisargaviasole <nisarg.jadhav@aviasole.com>
2025-01-29 18:59:10 +0530

    update sveltconfig.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
