b0f24b4ad97f61893a5ee13e970c31d6ac01b61b
nisargaviasole <nisarg.jadhav@aviasole.com>
2025-01-30 15:26:40 +0530

    change map

--- src/routes/user_geolocation/Map.svelte
+++ src/routes/user_geolocation/Map.svelte
@@ -1,7 +1,9 @@
 <script>
-// @ts-nocheck
-
+  // @ts-nocheck
   import { onMount, afterUpdate } from "svelte";
+  import maplibregl from "maplibre-gl";
+  import "maplibre-gl/dist/maplibre-gl.css";
+
   export let locations = [];
   export let defaultLatitude = 22.333836;
   export let defaultLongitude = 73.179375;
@@ -9,41 +11,62 @@
 
   let mapElement;
   let map;
+  let markers = [];
 
   onMount(() => {
-    import('maplibre-gl').then((L) => {
-      // Initialize Maplibre map
-      map = new L.Map(mapElement)
-        .setView([defaultLatitude, defaultLongitude], zoom)
-        .addLayer(new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
-
-      // Add markers
-      locations.forEach(location => {
-        L.marker([location.latitude, location.longitude])
-          .addTo(map)
-          .bindPopup(`Location: ${location.latitude}, ${location.longitude}`)
-          .openPopup();
-      });
+    map = new maplibregl.Map({
+      container: mapElement, // Correctly binding map to the HTML element
+      style: "https://demotiles.maplibre.org/style.json", // Free MapLibre tiles
+      center: [defaultLongitude, defaultLatitude],
+      zoom: zoom,
     });
+
+    map.addControl(new maplibregl.NavigationControl()); // Add zoom controls
+
+    updateMarkers(locations);
   });
+
+  function updateMarkers(locationArray) {
+    if (!map) return;
+
+    // Remove existing markers
+    markers.forEach(marker => marker.remove());
+    markers = [];
+
+    locationArray.forEach((location, index) => {
+      const marker = new maplibregl.Marker()
+        .setLngLat([location.longitude, location.latitude])
+        .setPopup(
+          new maplibregl.Popup({ offset: 25 }).setText(
+            `Location ${index + 1}: ${location.latitude}, ${location.longitude}`
+          )
+        )
+        .addTo(map);
+
+      markers.push(marker);
+    });
+
+    if (locationArray.length > 0) {
+      const bounds = new maplibregl.LngLatBounds();
+      locationArray.forEach(location => {
+        bounds.extend([location.longitude, location.latitude]);
+      });
+      map.fitBounds(bounds, { padding: 20 });
+    }
+  }
 
   afterUpdate(() => {
     if (map && locations.length > 0) {
-      map.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
-          map.removeLayer(layer);
-        }
-      });
-
-      locations.forEach(location => {
-        L.marker([location.latitude, location.longitude])
-          .addTo(map)
-          .bindPopup(`Location: ${location.latitude}, ${location.longitude}`)
-          .openPopup();
-      });
+      updateMarkers(locations);
     }
   });
 </script>
 
-<!-- svelte-ignore element_invalid_self_closing_tag -->
-<div bind:this={mapElement} class="map-container" />
+<div bind:this={mapElement} class="map-container"></div>
+
+<style>
+  .map-container {
+    height: 400px;
+    width: 100%;
+  }
+</style>
