5278360b94814cb9fa8a9241a67d413c61f60ee6
nisargaviasole <nisarg.jadhav@aviasole.com>
2025-01-30 11:18:55 +0530

    changes in configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: true,   // Allow network access
-    port: 3333    // Set the correct port
-  }
+	plugins: [sveltekit()]
 });
